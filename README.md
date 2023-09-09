2c0622930b29f8e9a77c25853f9bf860c965574f
Paulo Henrique Eli <@paulo.eli>
2023-09-09 19:17:30 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# ig_new_posts_downloader
+Certainly, here's the updated README.md with a "Piece of Advice" section:
+
+# Instagram Post Downloader
+
+This Python script allows you to download Instagram posts from specific user profiles and generate HTML pages for easy
+viewing. **Please note that the developer is not responsible for any misuse of this script**, and it should be used in
+compliance with Instagram's terms of service.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Developer Disclaimer](#developer-disclaimer)
+- [Piece of Advice](#piece-of-advice)
+
+## Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/yourusername/instagram-post-downloader.git
+   cd instagram-post-downloader
+   ```
+
+2. Create a virtual environment (optional but recommended):
+
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # On Windows, use `venv\Scripts\activate`
+   ```
+
+3. Install the required Python packages:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. Create a `.env` file in the project directory and add the following environment variables:
+
+   ```
+   username=your_instagram_username
+   password=your_instagram_password
+   posts=number_of_posts_to_download
+   profiles=profile1,profile2,profile3
+   ```
+
+   Replace `your_instagram_username` and `your_instagram_password` with your Instagram credentials, specify the number
+   of posts to download in `number_of_posts_to_download`, and list the profiles you want to download posts from
+   in `profile1,profile2,profile3`.
+
+## Usage
+
+1. Run the script:
+
+   ```bash
+   python instagram_post_downloader.py
+   ```
+
+   The script will log in to your Instagram account, download the specified number of posts from the specified profiles,
+   and generate HTML pages for each post.
+
+2. The downloaded posts and HTML pages can be found in the `profiles` directory in the project folder.
+
+## Developer Disclaimer
+
+The developer of this script is not responsible for any misuse of this tool. This script is intended for educational and
+personal use only. Please respect Instagram's terms of service and the privacy of other users when using this script.
+Use it responsibly and at your own risk.
+
+## Piece of Advice
+
+**Piece of Advice:** It's highly recommended not to use your primary Instagram account to log in when running this
+script. Create a separate Instagram account for this purpose to protect your primary account's credentials and privacy.
+Additionally, please pay attention to any potential security issues related to the `instagrapi` library, and ensure that
+you are using the latest version to minimize security risks.
---
+++ main.py
@@ -0,0 +1,114 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from instagrapi import Client
+from jinja2 import Environment, FileSystemLoader
+
+
+def load_configurations():
+    load_dotenv()
+
+    user = os.getenv("username")
+    pswd = os.getenv("password")
+    post_numbers = os.getenv("posts")
+    profiles_str = os.getenv("profiles")
+
+    if None in (user, pswd, profiles_str):
+        raise ValueError("One or more required environment variables are missing.")
+
+    profiles = profiles_str.split(",")
+    return {"user": user, "pswd": pswd, "profiles": profiles, "post": post_numbers}
+
+
+def connect(configurations: dict):
+    client = Client()
+    client.login(configurations["user"], configurations["pswd"])
+    return client
+
+
+def download_media(client: Client, media, path: Path):
+    if media.media_type == 1:
+        client.photo_download(media.pk, path)
+    elif media.media_type == 2:
+        client.video_download(media.pk, path)
+    elif media.media_type == 8:
+        client.album_download(media.pk, path)
+    return
+
+
+def get_profile_history(profile: str):
+    profile_folder = Path.cwd() / "profiles" / profile
+    history_file = profile_folder / "history.dat"
+
+    if not history_file.exists():
+        profile_folder.mkdir(parents=True, exist_ok=True)
+        with open(history_file, "w", encoding="utf-8") as file:
+            file.write("[]")
+        return []
+
+    with open(history_file, "r") as file:
+        history = file.read().strip("[]").split(", ")
+
+    return history
+
+
+def update_profile_history(profile: str, history: []):
+    path = Path.cwd() / "profiles" / profile / "history.dat"
+    with open(path, "w", encoding="utf-8") as file:
+        file.write("[" + ", ".join(history) + "]")
+    return
+
+
+def generate_html(caption_text: str, path: Path):
+    env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+    template = env.get_template('media_template.html')
+
+    media_urls = [f"{media_file}" for media_file in os.listdir(path) if not media_file.endswith('.txt')]
+    html_content = template.render(caption=caption_text, media_urls=media_urls)
+
+    with open(path / "index.html", "w", encoding="utf-8") as file:
+        file.write(html_content)
+
+    return
+
+
+def get_posts(client: Client, profile: str, post_numbers: int):
+    print(f"Getting posts from {profile}")
+
+    history = get_profile_history(profile)
+
+    user_id = client.user_id_from_username(profile)
+    medias = client.user_medias(user_id, post_numbers)
+
+    new_posts = []
+    for media in medias:
+        if media.code in history:
+            continue
+
+        new_posts.append(media.code)
+        history.append(media.code)
+
+        path = Path.cwd() / "profiles" / profile / media.code
+        path.mkdir(parents=True, exist_ok=True)
+
+        with open(path / "description.txt", "w", encoding="utf-8") as file:
+            file.write(media.caption_text)
+
+        download_media(client, media, path)
+        generate_html(media.caption_text, path)
+
+        print(f"{media.code} downloaded with success")
+
+    update_profile_history(profile, history)
+    print(f"{profile} {'has' if len(new_posts) > 0 else 'has no'} new posts")
+
+    return
+
+
+if __name__ == "__main__":
+    configurations = load_configurations()
+    client = connect(configurations)
+
+    for profile in configurations["profiles"]:
+        get_posts(client, profile, int(configurations["post"]))
