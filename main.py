26add1df4a2531ba4db0853c4d94f47032142432
Paulo Henrique Eli <@paulo.eli>
2023-10-17 20:54:13 -0300

    Create a file to control profiles have been processed in current day; `cached` the profile id; implemented method to handle with timeout error

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+import random
+import tempfile
+import time
+from datetime import datetime
 from pathlib import Path
 
 from dotenv import load_dotenv
 from instagrapi import Client
 from jinja2 import Environment, FileSystemLoader
 from tqdm import tqdm
-
-import os
 
 
 def load_configurations():
@@ -70,6 +72,24 @@ def get_profile_history(profile: str):
     return history
 
 
+def get_user_id_from_profile(profile: str, client: Client):
+    profile_folder = Path.cwd() / "profiles" / profile
+    info_file = profile_folder / "info"
+
+    if not info_file.exists():
+        user_id = client.user_id_from_username(profile)
+
+        profile_folder.mkdir(parents=True, exist_ok=True)
+        with open(info_file, "w", encoding="utf-8") as file:
+            file.write(user_id)
+        return user_id
+
+    with open(info_file, "r") as file:
+        user_id = file.read()
+
+    return user_id
+
+
 def update_profile_history(profile: str, history: []):
     path = Path.cwd() / "profiles" / profile / "history.dat"
     with open(path, "w", encoding="utf-8") as file:
@@ -90,41 +110,66 @@ def generate_html(caption_text: str, code: str, path: Path):
     return
 
 
-def get_posts(client: Client, profile: str, posts: int, new_user_posts: int, save_method: str):
+def get_day_processed_profiles():
+    temporary_file = os.path.join(tempfile.gettempdir(), datetime.now().strftime("%Y%m%d"))
+    if not os.path.exists(temporary_file):
+        with open(temporary_file, "w", encoding="utf-8") as file:
+            file.write("[]")
+        return []
+
+    with open(temporary_file, "r") as file:
+        return file.read().strip("[]").split(", ")
+
+
+def save_day_processed_profiles(profiles: list):
+    temporary_file = os.path.join(tempfile.gettempdir(), datetime.now().strftime("%Y%m%d"))
+    with open(temporary_file, "w") as file:
+        return file.write("[" + ", ".join(profiles) + "]")
+
+
+def get_posts(client: Client, profile: str, posts: int, new_user_posts: int, save_method: str, max_retries=3):
     history = get_profile_history(profile)
 
-    user_id = client.user_id_from_username(profile)
-    if not history:
-        medias = client.user_medias(user_id, new_user_posts)
-    else:
-        medias = client.user_medias(user_id, posts)
+    for retry in range(max_retries):
+        try:
+            user_id = get_user_id_from_profile(profile, client)
+            if not history:
+                medias = client.user_medias(user_id, new_user_posts)
+            else:
+                medias = client.user_medias(user_id, posts)
 
-    new_posts = []
-    for media in medias:
-        if media.code in history:
-            continue
+            new_posts = []
+            for media in medias:
+                if media.code in history:
+                    continue
 
-        new_posts.append(media.code)
-        history.append(media.code)
+                new_posts.append(media.code)
+                history.append(media.code)
 
-        if save_method == "new":
-            path = Path.cwd() / "new" / media.code
-        else:
-            path = Path.cwd() / "profiles" / profile / media.code
+                if save_method == "new":
+                    path = Path.cwd() / "new" / media.code
+                else:
+                    path = Path.cwd() / "profiles" / profile / media.code
 
-        path.mkdir(parents=True, exist_ok=True)
+                path.mkdir(parents=True, exist_ok=True)
 
-        with open(path / "description.txt", "w", encoding="utf-8") as file:
-            file.write(media.caption_text)
+                with open(path / "description.txt", "w", encoding="utf-8") as file:
+                    file.write(media.caption_text)
 
-        download_media(client, media, path)
-        generate_html(media.caption_text, media.code, path)
-        update_profile_history(profile, history)
+                download_media(client, media, path)
+                generate_html(media.caption_text, media.code, path)
+                update_profile_history(profile, history)
 
-    if save_method == "profile":
-        print(f"{profile} {'has' if len(new_posts) > 0 else 'has no'} new posts")
+            if save_method == "profile":
+                print(f"{profile} {'has' if len(new_posts) > 0 else 'has no'} new posts")
 
-    return
+            return
+        except ConnectionError:
+            if retry < max_retries - 1:
+                time.sleep(random.randint(60, 120))
+            else:
+                # If this is the last retry, raise the exception to stop the process
+                raise
 
 
 if __name__ == "__main__":
@@ -140,10 +185,21 @@ if __name__ == "__main__":
     user_posts = int(configurations["user_posts"])
     new_user_posts = int(configurations["new_user_posts"])
 
+    processed_profiles = get_day_processed_profiles()
     progress_bar = tqdm(desc="Processing Profiles", total=len(profiles), position=0, leave=True)
 
-    for idx, profile in enumerate(profiles):
-        get_posts(client, profile, user_posts, new_user_posts, save_method)
-        progress_bar.update()
+    try:
+        for idx, profile in enumerate(sorted(profiles)):
+            if profile in processed_profiles:
+                progress_bar.update()
+                continue
 
-    progress_bar.close()
+            get_posts(client, profile, user_posts, new_user_posts, save_method)
+            progress_bar.update()
+
+            processed_profiles.append(profile)
+    finally:
+        if processed_profiles is not None and len(processed_profiles) > 0:
+            save_day_processed_profiles(processed_profiles)
+
+        progress_bar.close()
